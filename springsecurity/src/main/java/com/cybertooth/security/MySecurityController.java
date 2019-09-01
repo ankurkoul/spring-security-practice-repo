7c9f1acaab16322de1d7e364554e9436016badee
er.ankurkoul <er.ankurkoul@gmail.com>
2019-09-01 14:38:45 +0530

    second commit--override default security by using in-memory user details manager

--- springsecurity/src/main/java/com/cybertooth/security/MySecurityController.java
+++ springsecurity/src/main/java/com/cybertooth/security/MySecurityController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class MySpringSecurityController {
+public class MySecurityController {
 
 	@RequestMapping("/")
 	public String hello() {
